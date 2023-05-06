c8163cab5ee2ddae42ef1040dd349c3f3547dec9
JmanuelJ <47757280+JmanuelJ@users.noreply.github.com>
2023-05-06 15:13:50 -0500

    Mejoras de interfaz
    Incorporación  de  material design

--- PracticaUno/app/src/main/java/com/practicauno/MainActivity.kt
+++ PracticaUno/app/src/main/java/com/practicauno/MainActivity.kt
@@ -5,11 +5,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private var number1: EditText? = null
     private var number2: EditText? = null
     private var result: TextView? = null
+    private var n1: String ? = null
+    private var n2: String ? = null
     private val op: Operation = Operation()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,15 +34,27 @@ class MainActivity : AppCompatActivity() {
         val subs: Int?
         val mul: Int?
         val div: Double?
-        number1?.let{
-            number2.let {
-                add = op.additionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
-                subs = op.subtractionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
-                mul = op.multiplicationOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
-                div = op.divisionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
-                result?.text = String.format(getString(R.string.calc_result),add,subs,mul,div)
+        n1 = number1?.getText().toString()
+        n2 = number2?.getText().toString()
+        if (n1 != "" && n2 != ""){
+            number1?.let{
+                number2?.let {
+                    add = op.additionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
+                    subs = op.subtractionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
+                    mul = op.multiplicationOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
+                    div = op.divisionOp(number1?.text.toString().toInt(), number2?.text.toString().toInt())
+                    result?.text = String.format(getString(R.string.calc_result),add,subs,mul,div)
+                }
             }
+        } else{
+            Toast.makeText(this, "Ingrese los 2 numeros", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun reset(view: View) {
+        result?.setText("")
+        number1?.setText("")
+        number2?.setText("")
 
     }
 }
--- PracticaUno/app/src/main/java/com/practicauno/Operation.kt
+++ PracticaUno/app/src/main/java/com/practicauno/Operation.kt
@@ -1,5 +1,7 @@
 package com.practicauno
 
+import android.view.View
+
 class Operation() {
     fun additionOp(numberOne: Int, numberTwo: Int): Int = numberOne + numberTwo
     fun subtractionOp(numberOne: Int, numberTwo: Int): Int = numberOne - numberTwo
